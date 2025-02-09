37efa6d5d03ef9a0b00225f4955f2e209e2424b5
Jonze123 <Balenor321@gmail.com>
2025-02-09 12:47:02 +0200

    Add vulkan device

---
+++ 3dDreams/vulkan_device.c
@@ -0,0 +1,11 @@
+#include "vulkan.h"
+#include "common.h"
+
+static bool vulkan_device_create(vulkan_context* context)
+{
+	return true;
+}
+
+static void vulkan_device_destroy(vulkan_context* context)
+{
+}
