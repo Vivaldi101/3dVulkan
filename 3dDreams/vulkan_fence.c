07881ae489c82d1b38678238f099b60da28557f4
Jonze123 <Balenor321@gmail.com>
2025-03-08 00:00:59 +0200

    Add vulkan fences

---
+++ 3dDreams/vulkan_fence.c
@@ -0,0 +1,51 @@
+#include "vulkan.h"
+#include "common.h"
+
+static b32 vulkan_fence_wait(vulkan_context* context, vulkan_fence* fence, u64 timeout)
+{
+   if(fence->is_signaled)
+      return true;
+
+   switch(vkWaitForFences(context->device.logical_device, 1, &fence->handle, true, timeout))
+   {
+      default:
+         return false;
+      case VK_SUCCESS:
+         return fence->is_signaled = true;
+   }
+
+   return true;
+}
+
+static b32 vulkan_fence_reset(vulkan_context* context, vulkan_fence* fence)
+{
+   if(!fence->is_signaled)
+      return true;
+
+   if(!VK_VALID(vkResetFences(context->device.logical_device, 1, &fence->handle)))
+      return false;
+
+   fence->is_signaled = false;
+   return true;
+}
+
+static b32 vulkan_fence_create(vulkan_context* context)
+{
+   for(u32 i = 0; i < VULKAN_MAX_FRAME_BUFFER_COUNT; ++i)
+   {
+      VkSemaphoreCreateInfo sema_info = {VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO};
+      if(!VK_VALID(vkCreateSemaphore(context->device.logical_device, &sema_info, context->allocator, &context->image_available_semaphores[i])))
+         return false;
+      if(!VK_VALID(vkCreateSemaphore(context->device.logical_device, &sema_info, context->allocator, &context->queue_complete_semaphores[i])))
+         return false;
+
+      VkFenceCreateInfo fence_info = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO};
+
+      if(!VK_VALID(vkCreateFence(context->device.logical_device, &fence_info, context->allocator, &context->in_flight_fences[i].handle)))
+         return false;
+
+      context->in_flight_fences[i].is_signaled = true;
+   }
+
+   return true;
+}
