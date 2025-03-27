97758516bb7167b8030400d37a55e4bcdc0d8d1f
Jonze123 <Balenor321@gmail.com>
2025-03-27 21:37:11 +0200

    Magenta clear screen

--- 3dDreams/vulkan_ng.c
+++ 3dDreams/vulkan_ng.c
@@ -9,18 +9,23 @@
 
 #pragma comment(lib,	"vulkan-1.lib")
 
-enum { VULKAN_MAX_FRAME_BUFFER_COUNT = 3, OBJECT_SHADER_COUNT = 2 };
+enum { OBJECT_SHADER_COUNT = 2 };
+
+// TODO: Why release wont build
 
 #define VK_VALID(v) ((v) == VK_SUCCESS)
+#define VK_TEST(v) if(!VK_VALID((v))) return false
+#define VK_TEST_HANDLE(v) if(!VK_VALID((v))) return VK_NULL_HANDLE
+
+#ifdef _DEBUG
 #define VK_ASSERT(v) \
         do { \
           VkResult r = (v); \
           assert(VK_VALID(r)); \
         } while(0)
-
-#define VK_TEST(v) if(!VK_VALID((v))) return false
-#define VK_TEST_HANDLE(v) if(!VK_VALID((v))) return VK_NULL_HANDLE
-#define VK_RETURN_HANDLE(r, v) VK_TEST(r) ? (v) : VK_NULL_HANDLE
+#else
+#define VK_ASSERT(v) (v)
+#endif
 
 align_struct
 {
@@ -31,17 +36,15 @@ align_struct
    VkSurfaceKHR surface;
    VkSwapchainKHR swapchain;
    VkAllocationCallbacks* allocator;
+   VkSemaphore image_ready_semaphore;
+   VkSemaphore image_done_semaphore;
+   VkQueue graphics_queue;
+   VkCommandPool command_pool;
+   VkCommandBuffer command_buffer;
+
+   VkImage swapchain_images[16]; // should be big enough
+   u32 swapchain_image_count;
 } vulkan_context;
-
-void vulkan_resize(void* renderer, u32 width, u32 height)
-{
-   // ...
-}
-
-void vulkan_present(hw* hw)
-{
-   // ...
-}
 
 static VkPhysicalDevice vulkan_pdevice_select(vulkan_context* context)
 {
@@ -68,12 +71,12 @@ static u32 vulkan_ldevice_select_index(vulkan_context* context)
    return 0;
 }
 
-static VkDevice vulkan_ldevice_select(vulkan_context* context, u32 family_index)
+static VkDevice vulkan_ldevice_create(vulkan_context* context, u32 queue_family_index)
 {
    f32 queue_prio = 1.0f;
 
    VkDeviceQueueCreateInfo queue_info = {VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO};
-   queue_info.queueFamilyIndex = family_index; // TODO: query the right queue family
+   queue_info.queueFamilyIndex = queue_family_index; // TODO: query the right queue family
    queue_info.queueCount = 1;
    queue_info.pQueuePriorities = &queue_prio;
 
@@ -85,14 +88,34 @@ static VkDevice vulkan_ldevice_select(vulkan_context* context, u32 family_index)
    ldev_info.ppEnabledExtensionNames = dev_ext_names;
 
    VkDevice dev;
-   VkResult r = vkCreateDevice(context->pdev, &ldev_info, context->allocator, &dev);
+   VK_TEST_HANDLE(vkCreateDevice(context->pdev, &ldev_info, context->allocator, &dev));
 
-   return VK_VALID(r) ? dev : 0;
+   return dev;
+}
+
+static VkQueue vulkan_graphics_queue_create(vulkan_context* context, u32 queue_family_index)
+{
+   VkQueue graphics_queue = 0;
+
+   // TODO: Get the queue index
+   vkGetDeviceQueue(context->ldev, queue_family_index, 0, &graphics_queue);
+
+   return graphics_queue;
+}
+
+static VkSemaphore vulkan_semaphore_create(vulkan_context* context)
+{
+   VkSemaphore sema = 0;
+
+   VkSemaphoreCreateInfo sema_info = {VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO};
+   VK_TEST_HANDLE(vkCreateSemaphore(context->ldev, &sema_info, context->allocator, &sema));
+
+   return sema;
 }
 
 static VkSwapchainKHR vulkan_swapchain_create(vulkan_context* context, u32 queue_family_index, u32 w, u32 h)
 {
-   VkSwapchainKHR swapchain;
+   VkSwapchainKHR swapchain = 0;
 
    VkSurfaceCapabilitiesKHR surface_caps;
    VK_TEST_HANDLE(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(context->pdev, context->surface, &surface_caps));
@@ -117,6 +140,8 @@ static VkSwapchainKHR vulkan_swapchain_create(vulkan_context* context, u32 queue
    if(image_count < 2)
       return 0;
 
+   context->swapchain_image_count = image_count;
+
    VkSwapchainCreateInfoKHR swapchain_info = {VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR};
    swapchain_info.surface = context->surface;
    swapchain_info.minImageCount = image_count; // triple buffering
@@ -134,9 +159,35 @@ static VkSwapchainKHR vulkan_swapchain_create(vulkan_context* context, u32 queue
    swapchain_info.clipped = VK_TRUE;
    swapchain_info.oldSwapchain = context->swapchain;
 
-   VkResult r = vkCreateSwapchainKHR(context->ldev, &swapchain_info, context->allocator, &swapchain);
+   VK_TEST_HANDLE(vkCreateSwapchainKHR(context->ldev, &swapchain_info, context->allocator, &swapchain));
 
-   return VK_VALID(r) ? swapchain : 0;
+   return swapchain;
+}
+
+VkCommandBuffer vulkan_command_buffer_create(vulkan_context* context)
+{
+   VkCommandBuffer buffer = 0;
+   VkCommandBufferAllocateInfo buffer_allocate_info = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO};
+
+   buffer_allocate_info.commandBufferCount = 1;
+   buffer_allocate_info.commandPool = context->command_pool;
+   buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+   VK_ASSERT(vkAllocateCommandBuffers(context->ldev, &buffer_allocate_info, &buffer));
+
+   return buffer;
+}
+
+VkCommandPool vulkan_command_pool_create(vulkan_context* context, u32 queue_family_index)
+{
+   VkCommandPool pool = 0;
+
+   VkCommandPoolCreateInfo pool_info = {VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
+   pool_info.queueFamilyIndex = queue_family_index;
+
+   VK_TEST_HANDLE(vkCreateCommandPool(context->ldev, &pool_info, context->allocator, &pool));
+
+   return pool;
 }
 
 static VkSurfaceKHR vulkan_window_surface_create(vulkan_context* context, const hw_window* window, const char** const extension_names, usize extension_count)
@@ -163,9 +214,68 @@ static VkSurfaceKHR vulkan_window_surface_create(vulkan_context* context, const 
    win32SurfaceInfo.hwnd = window->handle;
 
    VkSurfaceKHR surface;
-   VkResult r = vkWin32SurfaceFunction(context->instance, &win32SurfaceInfo, 0, &surface);
+   VK_TEST_HANDLE(vkWin32SurfaceFunction(context->instance, &win32SurfaceInfo, 0, &surface));
 
-   return VK_VALID(r) ? surface : 0;
+   return surface;
+}
+
+void vulkan_resize(void* renderer, u32 width, u32 height)
+{
+   // ...
+}
+
+void vulkan_present(vulkan_context* context)
+{
+   u32 image_index = 0;
+
+   VK_ASSERT(vkAcquireNextImageKHR(context->ldev, context->swapchain, UINT64_MAX, context->image_ready_semaphore, VK_NULL_HANDLE, &image_index));
+   VK_ASSERT(vkResetCommandPool(context->ldev, context->command_pool, 0));
+
+   VkCommandBufferBeginInfo buffer_begin_info = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
+   buffer_begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+   VkCommandBuffer buffer = context->command_buffer;
+
+   VK_ASSERT(vkBeginCommandBuffer(buffer, &buffer_begin_info));
+
+   VkClearColorValue color = {1, 0, 1, 1};
+
+   VkImageSubresourceRange range = {};
+   range.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+   range.layerCount = 1;
+   range.levelCount = 1;
+
+   vkCmdClearColorImage(buffer, context->swapchain_images[image_index], VK_IMAGE_LAYOUT_GENERAL, &color, 1, &range);
+
+   VK_ASSERT(vkEndCommandBuffer(buffer));
+
+   VkSubmitInfo submit_info = {VK_STRUCTURE_TYPE_SUBMIT_INFO};
+   submit_info.waitSemaphoreCount = 1;
+   submit_info.pWaitSemaphores = &context->image_ready_semaphore;
+
+   submit_info.pWaitDstStageMask = &(VkPipelineStageFlags) { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+
+   submit_info.commandBufferCount = 1;
+   submit_info.pCommandBuffers = &buffer;
+
+   submit_info.signalSemaphoreCount = 1;
+   submit_info.pSignalSemaphores = &context->image_done_semaphore;
+
+   VK_ASSERT(vkQueueSubmit(context->graphics_queue, 1, &submit_info, VK_NULL_HANDLE));
+
+   VkPresentInfoKHR present_info = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR};
+   present_info.swapchainCount = 1;
+   present_info.pSwapchains = &context->swapchain;
+
+   present_info.pImageIndices = &image_index;
+
+   present_info.waitSemaphoreCount = 1;
+   present_info.pWaitSemaphores = &context->image_done_semaphore;
+
+   VK_ASSERT(vkQueuePresentKHR(context->graphics_queue, &present_info));
+
+   // wait until all queue ops are done
+   VK_ASSERT(vkDeviceWaitIdle(context->ldev));
 }
 
 bool vulkan_initialize(hw* hw)
@@ -210,9 +320,16 @@ bool vulkan_initialize(hw* hw)
 
    u32 queue_family_index = vulkan_ldevice_select_index(context);
    context->pdev = vulkan_pdevice_select(context);
-   context->ldev = vulkan_ldevice_select(context, queue_family_index);
+   context->ldev = vulkan_ldevice_create(context, queue_family_index);
    context->surface = vulkan_window_surface_create(context, &hw->renderer.window, ext_names, ext_count);
    context->swapchain = vulkan_swapchain_create(context, queue_family_index, hw->renderer.window.width, hw->renderer.window.height);
+   context->image_ready_semaphore = vulkan_semaphore_create(context);
+   context->image_done_semaphore = vulkan_semaphore_create(context);
+   context->graphics_queue = vulkan_graphics_queue_create(context, queue_family_index);
+   context->command_pool = vulkan_command_pool_create(context, queue_family_index);
+   context->command_buffer = vulkan_command_buffer_create(context);
+
+   VK_TEST(vkGetSwapchainImagesKHR(context->ldev, context->swapchain, &context->swapchain_image_count, context->swapchain_images));
 
    // app callbacks
    hw->renderer.backends[vulkan_renderer_index] = context;
