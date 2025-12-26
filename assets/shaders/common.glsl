ab8dc33c0122688a72409834f80a7a12dcadf068
Jonze123 <Balenor321@gmail.com>
2025-12-26 14:28:53 +0200

    Plane equations

--- assets/shaders/common.glsl
+++ assets/shaders/common.glsl
@@ -28,4 +28,15 @@ vec3 color_to_linear(vec3 color)
    return pow(color, vec3(gamma));
 }
 
+struct plane
+{
+   vec3 n;  // any point x on the plane satisfy dot(n, x) = d
+   float d; // othogonal distance from the origin dot(n, p) = d for a fixed point p on the plane
+};
+
+float plane_distance(plane pl, vec3 v)
+{
+   return dot(pl.n, v) - pl.d;
+}
+
 #endif
